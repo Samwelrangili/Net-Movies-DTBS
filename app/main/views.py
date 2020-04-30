c0f7077264edc2778f18e426fc2ab52d4e392eeb
Samwelrangili <samwelrangili896@gmail.com>
2020-04-30 17:39:53 +0300

    registered a blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,12 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .requests import get_movies,get_movie,search_movie
+from . import main
+from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-from .models import reviews
-
-Review = reviews.Review
+from ..models import Review
 
 
-@app.route('/')
+# Views
+@main.route('/')
 def index():
 
     '''
@@ -24,12 +23,12 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('.search',movie_name=search_movie))
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -42,7 +41,8 @@ def movie(id):
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 
-@app.route('/search/<movie_name>')
+
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -50,12 +50,11 @@ def search(movie_name):
     movie_name_list = movie_name.split(" ")
     movie_name_format = "+".join(movie_name_list)
     searched_movies = search_movie(movie_name_format)
-
     # title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
 
     form = ReviewForm()
@@ -69,7 +68,7 @@ def new_review(id):
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
 
-        return redirect(url_for('movie',id = movie.id ))
+        return redirect(url_for('.movie',id = movie.id ))
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
---
+++ start.sh
@@ -0,0 +1,4 @@
+ export MOVIE_API_KEY=0c19df9435558aa40ec199d46a305d36
+export SECRET_KEY=0758597216
+
+python3.6 manage.py server
